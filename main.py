770a5d6361c7412a851dcb21ec23e1f806454508
Piotr Wesu <piotrwesu@gmail.com>
2026-02-14 19:57:34 +0100

    import constants

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import pygame
-import constants
 
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
 
-    player  = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    player  = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print("Starting Asteroids with pygame version: ", pygame.version.ver)
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
         log_state()
--- player.py
+++ player.py
@@ -1,15 +1,15 @@
 import pygame
-import constants
 
 from circleshape import CircleShape
+from constants import LINE_WIDTH, PLAYER_RADIUS
 
 class Player(CircleShape):
     def __init__(self, x, y):
-        super().__init__(x, y, constants.PLAYER_RADIUS)
+        super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
 def draw(self, screen):
-   pygame.draw.polygon(screen, "white", self.triangle(), constants.LINE_WIDTH) 
+   pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH) 
 
 def triangle(self):
     forward = pygame.Vector2(0, 1).rotate(self.rotation)

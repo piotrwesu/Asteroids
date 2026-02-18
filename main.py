9eddd5f2df23eeef9250928a9e700af2b2b3c668
Piotr Wesu <piotrwesu@gmail.com>
2026-02-18 21:58:03 +0100

    added collision

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
+import sys
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -14,15 +15,15 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     player  = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    asteroids = pygame.sprite.Group()
     Asteroid.containers = (asteroids, updatable, drawable)
 
     AsteroidField.containers = updatable 
-    astroField = AsteroidField()
+    asteroidField = AsteroidField()
 
     print("Starting Asteroids with pygame version: ", pygame.version.ver)
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -35,9 +36,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+        
+        for a in asteroids:
+            if a.collides_with(player):
+                log_event("player hit")
+                print("Game Over")
+                sys.exit()
+
         screen.fill("black")
 
-        updatable.update(dt)
         for d in drawable:
             d.draw(screen)
 
